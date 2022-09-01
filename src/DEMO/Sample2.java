87317d803e844f46cd4573a80008185259747d29
lenovo <lenovo@DESKTOP-A7R9JG9>
2022-09-01 11:21:09 +0530

    [sep project]

---
+++ src/DEMO/Sample2.java
@@ -0,0 +1,11 @@
+package DEMO;
+
+public class Sample2 
+{
+    public static void main(String[] args) 
+    
+    //not pushing in github
+    {
+		  System.out.println("All The Best Pratu");
+	}
+}
